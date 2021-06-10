dba801ec18f53c3b95dcf156fcb2e61b78225078
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2021-06-10 16:30:29 -0500

    intentando solucionaar el error

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -1,26 +1,26 @@
-// (function(){
-//     const formulario = document.querySelector('#formulario');
-//     formulario.addEventListener('submit', validarCliente);
+(function(){
+    const formulario = document.querySelector('#formulario');
+    formulario.addEventListener('submit', validarCliente);
 
-//     function validarCliente(e) {
-//         e.preventDefault();
+    function validarCliente(e) {
+        e.preventDefault();
 
-//         const nombre = document.querySelector('#nombre').value;
-//         const email = document.querySelector('#email').value;
-//         const telefono = document.querySelector('#telefono').value;
-//         const empresa = document.querySelector('#empresa').value;
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
 
-//         const cliente = { 
-//             nombre,
-//             email,
-//             telefono,
-//             empresa
-//         }
+        const cliente = { 
+            nombre,
+            email,
+            telefono,
+            empresa
+        }
 
-//         console.log(Object.values(cliente).every( input => input !== '' ));
+        console.log(Object.values(cliente).every( input => input !== '' ));
 
-//         // if (condition) {
+        if (condition) {
             
-//         // }
-//     }
-// })
+        }
+    }
+})
