332d7f9673d2108c31239a4fb65470a041cc77b4
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2021-06-11 18:57:24 -0500

    obtener un cliente para editar

--- js/API.js
+++ js/API.js
@@ -28,12 +28,23 @@ export const obtenerClientes = async () => {
     }
 }
 
-// Elimina un cliente...
+// Elimina un Cliente...
 export const eliminarCliente = async id => {
     try {
         await fetch (`${url}/${id}`,{
             method: 'DELETE'
         });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Obtiene un cliente por su ID
+export const obtenerCliente = async id => {
+    try {
+        const resultado = await fetch(`${url}/${id}`);
+        const cliente = await resultado.json();
+        return cliente;
     } catch (error) {
         console.log(error);
     }
