9aafdd8371992cd2acfecaa3a434d389ae1e1162
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2021-06-11 20:01:03 -0500

    Guardar cambios del cliente a editar

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,4 +1,5 @@
-import { obtenerCliente } from './API.js';
+import { obtenerCliente, editarCliente } from './API.js';
+import { mostrarAlerta, validar } from './funciones.js';
 
 (function() {
 
@@ -16,6 +17,10 @@ import { obtenerCliente } from './API.js';
 
         const cliente = await obtenerCliente(idCliente);
         mostrarCliente(cliente);
+
+        //Submit al formulario
+        const formulario = document.querySelector('#formulario');
+        formulario.addEventListener('submit', validarCliente);
     });
 
     function mostrarCliente(cliente) {
@@ -27,4 +32,25 @@ import { obtenerCliente } from './API.js';
         telefonoInput.value = telefono;
         idInput.value = id;
     }
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        const cliente = {
+            nombre: nombreInput.value, 
+            email: emailInput.value, 
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: parseInt(idInput.value)
+        }
+
+        if( validar(cliente) ) {
+            // Mostrar mensaje
+            mostrarAlerta('Todos los campos son obligatorios');
+        }
+
+        // Reescribe el objeto
+        editarCliente(cliente);
+    }
+
 })();
--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -1,4 +1,4 @@
-import { mostrarAlerta } from './funciones.js';
+import { mostrarAlerta, validar } from './funciones.js';
 import { nuevoCliente } from './API.js';
 
 (function() {
@@ -25,12 +25,8 @@ import { nuevoCliente } from './API.js';
             mostrarAlerta('todos los campos son obligatorios') 
             return;
         }
+
         nuevoCliente(cliente);
     }
 
-    function validar(obj) {
-        return !Object.values(obj).every(element => element !== '') ;
-    }
-
-   
 })();
